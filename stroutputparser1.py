5b925b5080d8aea79eb3a175d8749d4901781dbd
victor janni <jannivictor@gmail.com>
2025-06-25 15:59:25 +0530

    output parser

--- stroutputparser1.py
+++ stroutputparser1.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+
 load_dotenv()
 
 model = ChatOpenAI()
